ad492ab51802944b0ac202ad2d901c53796b78dd
Arun Nair <arungn@gmail.com>
2017-02-16 10:14:49 +0000

    Rearrange projects

--- string-utils/src/main/java/com/company/Strings.java
+++ string-utils/src/main/java/com/company/Strings.java
@@ -6,4 +6,8 @@ public class Strings {
 
     public static final Function<String, Boolean> isNumber = string -> string.chars().allMatch(Character::isDigit);
 
+    public static final Function<String, Boolean> isPalindrome = string -> {
+        return new StringBuilder(string).reverse().toString().equals(string);
+    };
+
 }
--- string-utils/src/test/java/com/company/StringsTest.java
+++ string-utils/src/test/java/com/company/StringsTest.java
@@ -11,13 +11,31 @@ import static org.junit.Assert.assertTrue;
 public class StringsTest {
 
     @Test
+    public void shouldCheckPalindrome() {
+        String str = "amma";
+        assertTrue(Optional.of(str).map(Strings.isPalindrome).get());
+
+        str = "111111111";
+        assertTrue(Optional.of(str).map(Strings.isPalindrome).get());
+    }
+
+    @Test
+    public void shouldCheckNonPalindrome() {
+        String str = "abcd";
+        assertFalse(Optional.of(str).map(Strings.isPalindrome).get());
+        
+        str = "123456543215";
+        assertFalse(Optional.of(str).map(Strings.isPalindrome).get());
+    }
+
+    @Test
     public void shouldStringContainOnlyNumberReturnTrue() {
         String str = "123123";
 
         Optional<Boolean> result = Stream.of(str).map(Strings.isNumber).findAny();
         assertTrue(result.get());
     }
-    
+
     @Test
     public void shouldStringContainNonNumberReturnFalse() {
         String str = "123sfdfs123";
@@ -25,7 +43,7 @@ public class StringsTest {
         Optional<Boolean> result = Stream.of(str).map(Strings.isNumber).findAny();
         assertFalse(result.get());
     }
-    
+
     @Test
     public void shouldStringContainSpecialCharsrReturnFalse() {
         String str = "12£%!3";
